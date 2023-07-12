80c9d1dc6c54826c034220cc523d9c8035cfd196
mmusica <mmusica@fina.hr>
2023-07-12 09:57:38 +0200

    Changed to version 17, added ProjectRepository

--- ppmtool/src/main/java/github/mmusica/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/github/mmusica/ppmtool/domain/Project.java
@@ -1,5 +1,34 @@
 package github.mmusica.ppmtool.domain;
 
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Entity
 public class Project {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String projectName;
+    private String projectIdentifier;
+    private String description;
+    private Date start_date;
+    private Date end_date;
+
+    private Date created_At;
+    private Date updated_At;
+
+    @PrePersist
+    protected void onCreate(){
+        this.created_At = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate(){
+        this.updated_At = new Date();
+    }
+
+
 
 }
