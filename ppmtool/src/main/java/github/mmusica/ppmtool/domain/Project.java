b47a329c22c55fac4264433b0062abbf792ce396
mmusica <mmusica@fina.hr>
2023-07-12 11:21:37 +0200

    Added basic Project validation

--- ppmtool/src/main/java/github/mmusica/ppmtool/domain/Project.java
+++ ppmtool/src/main/java/github/mmusica/ppmtool/domain/Project.java
@@ -1,6 +1,9 @@
 package github.mmusica.ppmtool.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import jakarta.persistence.*;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Getter;
 import lombok.Setter;
 
@@ -12,24 +15,40 @@ public class Project {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     @Getter
     @Setter
+    @NotBlank(message = "Project name cannot be blank")
     private String projectName;
+
     @Getter
     @Setter
+    @NotBlank(message = "Project Identifier cannot be blank")
+    @Size(min = 4, max = 5, message = "Please use 4 to 5 characters")
+    @Column(updatable = false, unique = true)
     private String projectIdentifier;
+
     @Getter
     @Setter
+    @NotBlank(message = "Project description cannot be blank")
     private String description;
+
     @Getter
     @Setter
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date start_date;
+
     @Getter
     @Setter
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date end_date;
+
     @Getter
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date created_At;
+
     @Getter
+    @JsonFormat(pattern = "yyyy-mm-dd")
     private Date updated_At;
 
     @PrePersist
