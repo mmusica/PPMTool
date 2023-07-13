4f8f5bb39d7d47da69e026227a2d9b2c20734424
mmusica <mmusica@fina.hr>
2023-07-13 09:23:23 +0200

    Created header and project item

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
-import Dashboard from './components/Dashboard';
+import "./App.css";
+import Dashboard from "./components/Dashboard";
+import Header from "./components/Layout/Header";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Dashboard></Dashboard>
     </div>
   );
